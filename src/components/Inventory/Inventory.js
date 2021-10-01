6ff20c8fd6b4c02a7c648f907921806409b010aa
reznuhasan <reznuhasan31@gmail.com>
2021-10-01 17:07:52 +0600

    inventory updated

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>This is Inventory Page</h2>
+            <h2>This is Inventory Extra Page</h2>
         </div>
     );
 };
